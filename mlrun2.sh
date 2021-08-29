e74868ad7aab3229bb040d1ae1852852e286e68c
kimtomato <wltn5195@naver.com>
2021-08-30 04:57:54 +0900

    OpenCV tutorial

--- mlrun2.sh
+++ mlrun2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo docker run -it --privileged --runtime nvidia --network host -v /home/kimtomato/project:/project \
-        nvcr.io/nvidia/l4t-ml:r32.5.0-py3
+        --env DISPLAY='192.168.123.104:0' nvcr.io/nvidia/l4t-ml:r32.5.0-py3
